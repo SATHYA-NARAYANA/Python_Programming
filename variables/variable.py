245d86332b41fb78c865cddb60a962070e5dccf5
Sathya-Narayana <sathya.learningworks@gmail.com>
2025-07-22 16:49:57 +0530

    variable added

---
+++ variables/variable.py
@@ -0,0 +1,16 @@
+a = "Anish"
+b = 100
+c = a
+
+
+print(a)
+print(type(a))
+
+''' Variable are nothing but a empty container that can be used to store data 
+
+    we can able to store multiple type os data in a variable , but at a time we can eble to store one value at a time , but if we need to store multiple types of to a singe variable . then this is possible in pyton, python give us a facility to store multiple types of data inside a single variable . we can aslo do various operations on it.
+
+    
+What are they , How is it possible ?
+
+    '''
