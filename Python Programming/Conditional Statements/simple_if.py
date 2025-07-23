296e030cbf8147f5593488ff17bfda652319950c
Sathya-Narayana <sathya.learningworks@gmail.com>
2025-07-23 16:19:57 +0530

    working

---
+++ Python Programming/Conditional Statements/simple_if.py
@@ -0,0 +1,10 @@
+x = input('Enter a character:\n')
+
+if x.isalpha():
+    print('is a character string')
+
+elif x.isdigit():
+    print('is a numeric data')
+
+else:
+    print('is a special character') 
