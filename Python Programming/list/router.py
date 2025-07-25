2ad90c3e8c41f6db2fc793bae9de68027d35dac0
Sathya-Narayana <sathya.learningworks@gmail.com>
2025-07-25 18:38:21 +0530

    learning

---
+++ Python Programming/list/router.py
@@ -0,0 +1,5 @@
+x = True - True + False
+y = x + 1
+print(x + y)
+print(x * y)
+print(x - y)
---
+++ Python Programming/list/user_input_list.py
@@ -0,0 +1,8 @@
+names = []
+while True:
+    name = input("Enter a name or (Type Quit or Enter 1 to Quit to exit)")
+    if name.lower() == 'quit' or 1:
+        break
+    names.append(name)
+print("List store names are:", name)
+    
