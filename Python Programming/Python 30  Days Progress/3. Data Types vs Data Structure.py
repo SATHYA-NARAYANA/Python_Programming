ea1aba503dade68a98ee215ee761423990477e7d
Sathya-Narayana <sathya.learningworks@gmail.com>
2025-07-27 23:07:04 +0530

    List Data Structure

--- Python Programming/Python 30  Days Progress/3. Data Types vs Data Structure.py
+++ Python Programming/Python 30  Days Progress/3. Data Types vs Data Structure.py
@@ -1,1 +1,21 @@
 print ("What other data types are there in Python ?")
+
+
+
+# String Data Type
+x = "Sathya Narayana"
+y = 'Loves'
+z = 'Ansible'
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# We can store data, but if we to store multiple data typea nd need to organiuze data for this we have to use Data Structures.
+
+# List Data Structure: type of data structure supported in python to organize and access data in order
+
+example = [1,2,3,4,5]
+
+print(type(example))
