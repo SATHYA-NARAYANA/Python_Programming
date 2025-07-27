3ad17dbe5e571f16a0f1a75d99e158b528cf045c
Sathya-Narayana <sathya.learningworks@gmail.com>
2025-07-27 22:40:30 +0530

    different types of data types in Python

---
+++ Python Programming/Python 30  Days Progress/data_types.py
@@ -0,0 +1,15 @@
+# Different data types in Python
+
+
+# Intiger data type
+
+x = 10
+y = 20.0
+z = 1 +90j
+
+print("Intiger data type:", type(x))
+print("Float data type:", type(y))
+print("Complex data type:", type(z))    
+
+
+
