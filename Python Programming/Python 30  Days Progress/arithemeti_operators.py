865670c515cbbd48964f8bb5816c72c3d21f0ee4
Sathya-Narayana <sathya.learningworks@gmail.com>
2025-07-27 22:22:04 +0530

    Arithemetic Operator added

---
+++ Python Programming/Python 30  Days Progress/arithemeti_operators.py
@@ -0,0 +1,15 @@
+# Python Arithmetic operations
+2
+x = int(input("Enter a value to x :"))
+y = int(input("Enter a value to y :"))
+
+
+print (f"   Arithmetic Operation Addition(+)             : {x + y}")
+print (f"   Arithmetic Operation Subtraction(-)          : {x - y}")
+print (f"   Arithmetic Operation Multiplication(*)       : {x * y}")
+print (f"   Arithmetic Operation Division(/)             : {x / y}")
+print (f"   Arithmetic Operation Modulus(%)              : {x % y}")
+print (f"   Arithmetic Operation Exponential(**)         : {x ** y}")
+print (f"   Arithmetic Operation Floor division(//)      : {x // y}")
+
+
