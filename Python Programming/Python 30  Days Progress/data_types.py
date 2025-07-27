3f931405ad6dda831fb764ed1c1f70efd2a43e73
Sathya-Narayana <sathya.learningworks@gmail.com>
2025-07-27 22:47:33 +0530

    why do we have different data types in python

--- Python Programming/Python 30  Days Progress/data_types.py
+++ Python Programming/Python 30  Days Progress/data_types.py
@@ -13,3 +13,13 @@ print("Complex data type:", type(z))
 
 
 
+# By default all the data inputted by user is string data type
+# To convert string to intiger we use int() function
+# To convert string to float we use float() function
+# To convert string to complex we use complex() function
+x = int(input("Enter a value to x :"))
+y = float(input("Enter a value to y :"))    
+z = complex(input("Enter a value to z :"))
+
+
+
