b4be7c08e8b447c7bf68dbe40dda1bc17e5303f6
Sathya-Narayana <sathya.learningworks@gmail.com>
2025-07-29 16:26:05 +0530

    shopping_cart program dded

---
+++ Python Programming/OOPS/CLASS/shopping_cart.py
@@ -0,0 +1,12 @@
+class Shopping_cart():
+
+    def __init__(self):
+        self.items = []
+
+    def add_items(self,name, price, quantity=1):
+        self.items.append({'name': name})
+
+
+cart = Shopping_cart()
+        
+    
